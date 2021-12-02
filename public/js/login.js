d26a90753e86629743482a8eb624d7a8b4319768
cairney98 <mcairney92@gmail.com>
2021-12-02 13:22:30 +0000

    Added a username contraint for if it is already taken by another user

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,16 @@
+const socket = io();
+
 const login = document.getElementById("login");
 const input = document.getElementById("username-input");
 const warning = document.getElementById("warning");
+
+let users = [];
+
+// get current usernames
+socket.emit("request usernames");
+socket.on("usernames", (data) => {
+  users = data;
+});
 
 // set username constraints
 login.addEventListener("submit", (e) => {
@@ -8,5 +18,8 @@ login.addEventListener("submit", (e) => {
   if (input.value.length < 3 || input.value.length > 10) {
     e.preventDefault();
     warning.innerText = "Username must be 3-10 characters.";
+  } else if (users.some((user) => user === input.value)) {
+    e.preventDefault();
+    warning.innerText = "Username taken, try another.";
   }
 });
--- server.js
+++ server.js
@@ -5,18 +5,21 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-// SERVER STATIC CONTENT
+// serve static pages
 app.use(express.static("public/css"));
 app.use(express.static("public/js"));
 
+// get login home page
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
+// get chat page
 app.get("/chat.html", (req, res) => {
   res.sendFile(__dirname + "/public/chat.html");
 });
 
+// set port for heroku server and local server
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
@@ -30,22 +33,28 @@ server.listen(port, () => {
 let users = [];
 io.on("connection", (socket) => {
   socket.on("user joined", (data) => {
-    // update list of online users 
-    users.push(data)
 
-    // send list of online users to the client
+    // update list of online users
+    users.push(data);
+
+    // send list of online users to all clients
     io.emit("online status", users);
-   
+
     // only user recieves welcome message
     socket.emit("connect-message", "Welcome " + data + "!");
 
     // update list of online users
     socket.on("disconnect", () => {
-      users = users.filter(user => user != data)
+      users = users.filter((user) => user != data);
     });
   });
 
-  // send user messages to all clients
+  // send taken usernames to login page
+socket.on("request usernames", () => {
+  socket.emit("usernames", users)
+})
+
+  // send messages to every other client
   socket.on("chat message", (msg) => {
     socket.broadcast.emit("chat message", msg);
   });
