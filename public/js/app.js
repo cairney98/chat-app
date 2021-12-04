a528e3aec2e19390dc2fa8028ffa32e3a8f94f26
cairney98 <mcairney92@gmail.com>
2021-12-04 12:58:09 +0000

    implemented jQuery

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,5 @@
 const socket = io();
 
-// create dom elements
-const messages = document.getElementById("messages");
-const online = document.getElementById("online");
-const form = document.getElementById("input-bar");
-const input = document.getElementById("input");
-
 // extract username from url
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
@@ -15,75 +9,74 @@ const userName = urlParams.get("username");
 socket.emit("user joined", userName);
 
 // send messages to server
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  if (input.value) {
-    socket.emit("chat message", { message: input.value, username: userName, time: new Date().toLocaleTimeString().substr(0,5)  });
+$(document).ready(function () {
+  $("#input-bar").submit((e) => {
+    e.preventDefault();
+    if ($("#input").val()) {
+      socket.emit("chat message", {
+        message: $("#input").val(),
+        username: userName,
+        time: new Date().toLocaleTimeString().substr(0, 5),
+      });
 
-    // display messages sent by user
-    const item = document.createElement("li");
-    const text = document.createElement("p");
-    const tag = document.createElement("p");
-    item.classList.add("message-container");
-    text.classList.add("sent-message");
-    tag.classList.add("sent-name");
-    text.textContent = input.value;
-    tag.textContent = "You · " + new Date().toLocaleTimeString().substr(0,5);
-    item.appendChild(tag);
-    item.appendChild(text);
-    messages.appendChild(item);
-    item.scrollIntoView();
-
-    // reset input text when sent
-    input.value = "";
-  }
+      // display messages sent by user
+      const item = $("<li></li>");
+      const text = $("<p></p>").text($("#input").val());
+      const tag = $("<p></p>").text(
+        "You · " + new Date().toLocaleTimeString().substr(0, 5)
+      );
+      $("#input").val("");
+      item.addClass("message-container");
+      text.addClass("sent-message");
+      tag.addClass("sent-name");
+      item.append(tag);
+      item.append(text);
+      $("#messages").append(item);
+      $(".message-container").last()[0].scrollIntoView();
+    }
+  });
 });
 
 // recieve and display messages sent by other users
 socket.on("chat message", function (msg) {
-  const item = document.createElement("li");
-  const tag = document.createElement("p");
-  const text = document.createElement("p");
-  item.classList.add("message-container");
-  tag.classList.add("message-name");
-  text.classList.add("message");
-  text.textContent = msg.message;
-  tag.textContent = msg.username + " · " + msg.time;
-  item.appendChild(tag);
-  item.appendChild(text);
-  messages.appendChild(item);
-  item.scrollIntoView();
+  const item = $("<li></li>");
+  const text = $("<p></p>").text(msg.message);
+  const tag = $("<p></p>").text(msg.username + " · " + msg.time);
+  item.addClass("message-container");
+  text.addClass("message");
+  tag.addClass("message-name");
+  item.append(tag);
+  item.append(text);
+  $("#messages").append(item);
+  $(".message-container").last()[0].scrollIntoView();
 });
 
 // recieve connection messages
 socket.on("connect-message", (data) => {
-  const item = document.createElement("p");
-  const name = document.createElement("p");
-  item.classList.add("connection-message");
-  item.textContent = data;
-  name.textContent = data.online;
-  messages.appendChild(item);
-  online.appendChild(name);
+  const item = $("<p></p>").text(data);
+  const name = $("<p></p>").text(data.online);
+  item.addClass("connection-message");
+  $("#messages").append(item);
+  $("#online").append(name);
   window.scrollTo(0, document.body.scrollHeight);
 });
 
 // update the display of online users
 socket.on("online status", (users) => {
-  online.innerHTML = "";
+  $("#online").html("");
   users.map((user) => {
-    const item = document.createElement("li");
-    const name = document.createElement("p");
-    const meta = document.createElement("caption");
+    const item = $("<li></li>");
+    const name = $("<p></p>");
+    const meta = $("<caption></caption>");
     if (user === userName) {
-      name.innerHTML = "<strong>" + user + "</strong>" + " (You)" ;
-      item.appendChild(name)
+      name.html("<strong>" + user + "</strong>" + " (You)");
+      item.append(name);
     } else {
-      name.innerHTML = "<strong>" + user + "</strong>" + "<br/>" ;
-      meta.innerText = "Joined 1min ago";
-      item.appendChild(name)
-      item.appendChild(meta);
+      name.html("<strong>" + user + "</strong>" + "<br/>");
+      meta.text("Joined 1min ago");
+      item.append(name);
+      item.append(meta);
     }
-
-    online.appendChild(item);
+    $("#online").append(item);
   });
 });
